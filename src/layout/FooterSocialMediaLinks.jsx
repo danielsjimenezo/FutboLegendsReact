89dea3cb080cf5e1d0068991c1bfa00d9f98d2aa
dugalcedo <107317025+dugalcedo@users.noreply.github.com>
2025-05-20 14:45:44 +0200

    footer links

--- src/layout/FooterSocialMediaLinks.jsx
+++ src/layout/FooterSocialMediaLinks.jsx
@@ -1,11 +1,41 @@
 const links = [
     {
-        icon: 
+      icon: "/images/Icons/",
+      alt: "LinkedIn",
+      href: ""
+    },
+    {
+      icon: "/images/Icons/",
+      alt: "Instagram",
+      href: ""
+    },
+    {
+      icon: "/images/Icons/",
+      alt: "Twitter",
+      href: ""
+    },
+    {
+      icon: "/images/Icons/",
+      alt: "Facebook",
+      href: ""
+    },
+    {
+      icon: "/images/Icons/tiktok.svg",
+      alt: "Tik Tok",
+      href: ""
     }
 ]
 
 function FooterSocialMediaLinks() {
-  return <></>;
+  return (
+    <nav className="social-media-links">
+      {links.map(link => (
+        <a href={link.href} key={link.href}>
+          <img src={link.icon} alt={link.alt} />
+        </a>
+      ))}
+    </nav>
+  )
 }
 
 export default FooterSocialMediaLinks;
