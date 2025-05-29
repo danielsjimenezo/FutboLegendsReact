bc842d1caa8e7cadc235b8f26aa37ac94888453e
mgomezu <mgomezu@unal.edu.co>
2025-05-29 15:43:06 -0500

    Stats teams table update

--- src/tables/StatsTeamsTable.jsx
+++ src/tables/StatsTeamsTable.jsx
@@ -1,13 +1,13 @@
 import Table from "./Table.jsx";
 
 const teams = [
-  `M. United`,
-  `R. Madrid`,
+  `Manchester United`,
+  `Real Madrid`,
   `Barcelona`,
-  `M. City`,
+  `Manchester City`,
   `Chelsea`,
   `Arsenal`,
-  `A. Madrid`,
+  `Atletico Madrid`,
   `Sevilla`,
   `Liverpool`,
   `Tottenham`,
@@ -15,15 +15,14 @@ const teams = [
   `Marseille`,
   `Monaco`,
   `Lyon`,
-  `Bayern`,
-  `B. Dortmund`,
-  `B. Leverkusen`,
+  `Bayern Munich`,
+  `Dortmund`,
+  `Bayer Leverkussen`,
   `Juventus`,
-  `A. Milan`,
-  `I. Milan`,
+  `Milan`,
   `Napoli`,
-  `A. Roma`,
-  `Ajax`,
+  `Roma`,
+  `Ajax`, 
   `Spain`,
   `France`,
   `Italy`,
@@ -36,6 +35,108 @@ const teams = [
   `Uruguay`,
   `Colombia`,
 ];
+
+const items = [
+  {
+    league: "Premier League",
+    team: "",
+    badge: {
+      type: "img",
+      src: "/images/Competitions/PremierLeague.png",
+    },
+    wins: 2,
+    mp: 2,
+    g: 1,
+    a: 4,
+    gc: 7,
+    "gc / mp": 92,
+    cc: 45,
+    dc: 12,
+  },
+  {
+    league: "La Liga",
+    team: "",
+    badge: {
+      type: "img",
+      src: "/images/Competitions/LaLiga.png",
+    },
+    wins: 2,
+    mp: 2,
+    g: 1,
+    a: 4,
+    gc: 7,
+    "gc / mp": 92,
+    cc: 45,
+    dc: 12,
+  },
+  {
+    league: "Bundesliga",
+    team: "",
+    badge: {
+      type: "img",
+      src: "/images/Competitions/Bundesliga.png",
+    },
+    wins: 2,
+    mp: 2,
+    g: 1,
+    a: 4,
+    gc: 7,
+    "gc / mp": 92,
+    cc: 45,
+    dc: 12,
+  },
+  {
+    league: "Ligue 1",
+    team: "",
+    badge: {
+      type: "img",
+      src: "/images/Competitions/Ligue1.png",
+    },
+    wins: 2,
+    mp: 2,
+    g: 1,
+    a: 4,
+    gc: 7,
+    "gc / mp": 92,
+    cc: 45,
+    dc: 12,
+  },
+  {
+    league: "Serie A",
+    team: "",
+    badge: {
+      type: "img",
+      src: "/images/Competitions/SerieA.png",
+    },
+    wins: 2,
+    mp: 2,
+    g: 1,
+    a: 4,
+    gc: 7,
+    "gc / mp": 92,
+    cc: 45,
+    dc: 12,
+  },
+  {
+    league: "Champions League",
+    team: "",
+    badge: {
+      type: "img",
+      src: "/images/Competitions/ChampionsLeague.png",
+    },
+    wins: 2,
+    mp: 2,
+    g: 1,
+    a: 4,
+    gc: 7,
+    "gc / mp": 92,
+    cc: 45,
+    dc: 12,
+  },
+
+  
+  ];
+
 
 const fakeRow = {
   id: 0,
@@ -62,14 +163,42 @@ for (let i = 0; i < 35; i++)
     team: teams[i],
   });
 
+  const expandables = []
+
+  console.log(items)
+
+  items.forEach((item, i) => {
+    const expandable = {
+      type: 'moreRows',
+      items: []
+    }
+    
+    for (let j = 0; j < teams.length ; j++) {
+      const values = Object.values(fakeRow)
+      const row = [
+        "",
+        teams[j],
+        { type: "img", src: `/images/Teams/${teams[j]}.png`, alt: teams[j] }, 
+        ...values.slice(3)
+      ]
+      expandable.items.push(row)
+    }
+    expandables.push(expandable)
+    console.log(expandable)
+  })
+
+
+
 function StatsTeams() {
+
   return (
     <Table
-      headings={Object.keys(fakeRow).filter((h) => h !== "id")}
-      items={fakeRows}
+      headings={Object.keys(items[0]).filter((h) => h !== "id")}
+      items={items }
       _key={"id"}
       hide={["id"]}
       id="stats-teams-table"
+      expandables={expandables}
       perPage={35}
       columnWidths={[
         "10%",
@@ -83,8 +212,22 @@ function StatsTeams() {
         "10%",
         "10%",
       ]}
+      totals={[
+        'All leagues',
+        'All',
+        100,
+        100,
+        100,
+        100,
+        100,
+        100,
+        100,
+        100,
+        100
+      ]}
       cellPaddingY="5.75px"
     />
+    
   );
 }
 
